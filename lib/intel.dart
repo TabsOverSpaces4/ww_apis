8e9f0e3a36ca8e21d71319e9c31d6d236c2ae47f
TabsOverSpaces4 <guptaharsh.2401@gmail.com>
2021-10-25 22:54:49 +0530

    NEw page added

---
+++ lib/intel.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Life extends StatefulWidget {
+
+
+  @override
+  _LifeState createState() => _LifeState();
+}
+
+class _LifeState extends State<Life> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+      
+    );
+  }
+}
